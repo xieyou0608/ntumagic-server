d622a580aceb6a73c4ceb5dd468b6f367dc32f0c
項于 <zxczxc11161116@gmail.com>
2025-05-07 11:10:01 +0800

    added seats on the second floor

--- routes/generate_seats.js
+++ routes/generate_seats.js
@@ -55,6 +55,7 @@ function generate_seats() {
   //   s.position = { row: s.row, col: s.col };
   // }
   let seats = [];
+  // first floor
   // row 1
   for (let j = 29; j >= 23; j -= 2) seats.push({ area: "X", row: 1, col: j, sold: 0 });
   for (let j = 21; j >= 9; j -= 2) seats.push({ area: "S", row: 1, col: j, sold: 0 });
@@ -84,8 +85,8 @@ function generate_seats() {
 
   //row 3
   for (let j = 31; j >= 27; j -= 2) seats.push({ area: "X", row: 3, col: j, sold: 0 });
-  for (let j = 25; j >= 21; j -= 2) seats.push({ area: "B", row: 3, col: j, sold: 0 });
-  for (let j = 19; j >= 11; j -= 2) seats.push({ area: "A", row: 3, col: j, sold: 0 });
+  for (let j = 25; j >= 19; j -= 2) seats.push({ area: "B", row: 3, col: j, sold: 0 });
+  for (let j = 17; j >= 11; j -= 2) seats.push({ area: "A", row: 3, col: j, sold: 0 });
   seats.push({ area: "M", row: 3, col: 0, sold: 0 });
   for (let j = 0; j < 4; j++) seats.push({ area: "X", row: 3, col: j, sold: 0 });
   seats.push({ area: "M", row: 3, col: 0, sold: 0 });
@@ -94,14 +95,14 @@ function generate_seats() {
   seats.push({ area: "M", row: 3, col: 0, sold: 0 });
   for (let j = 0; j < 5; j++) seats.push({ area: "X", row: 3, col: j, sold: 0 });
   seats.push({ area: "M", row: 3, col: 0, sold: 0 });
-  for (let j = 10; j <= 18; j += 2) seats.push({ area: "A", row: 3, col: j, sold: 0 });
-  for (let j = 20; j <= 24; j += 2) seats.push({ area: "B", row: 3, col: j, sold: 0 });
+  for (let j = 10; j <= 16; j += 2) seats.push({ area: "A", row: 3, col: j, sold: 0 });
+  for (let j = 18; j <= 24; j += 2) seats.push({ area: "B", row: 3, col: j, sold: 0 });
   for (let j = 26; j <= 30; j += 2) seats.push({ area: "X", row: 3, col: j, sold: 0 });
 
   // row 4
   for (let j = 31; j >= 27; j -= 2) seats.push({ area: "X", row: 4, col: j, sold: 0 });
-  for (let j = 25; j >= 23; j -= 2) seats.push({ area: "B", row: 4, col: j, sold: 0 });
-  for (let j = 21; j >= 11; j -= 2) seats.push({ area: "A", row: 4, col: j, sold: 0 });
+  for (let j = 25; j >= 21; j -= 2) seats.push({ area: "B", row: 4, col: j, sold: 0 });
+  for (let j = 19; j >= 11; j -= 2) seats.push({ area: "A", row: 4, col: j, sold: 0 });
   seats.push({ area: "M", row: 4, col: 0, sold: 0 });
   for (let j = 0; j < 4; j++) seats.push({ area: "X", row: 4, col: j, sold: 0 });
   seats.push({ area: "M", row: 4, col: 0, sold: 0 });
@@ -110,14 +111,14 @@ function generate_seats() {
   seats.push({ area: "M", row: 4, col: 0, sold: 0 });
   for (let j = 0; j < 4; j++) seats.push({ area: "X", row: 4, col: j, sold: 0 });
   seats.push({ area: "M", row: 4, col: 0, sold: 0 });
-  for (let j = 12; j <= 22; j += 2) seats.push({ area: "A", row: 4, col: j, sold: 0 });
-  for (let j = 24; j <= 26; j += 2) seats.push({ area: "B", row: 4, col: j, sold: 0 });
+  for (let j = 12; j <= 20; j += 2) seats.push({ area: "A", row: 4, col: j, sold: 0 });
+  for (let j = 22; j <= 26; j += 2) seats.push({ area: "B", row: 4, col: j, sold: 0 });
   for (let j = 28; j <= 32; j += 2) seats.push({ area: "X", row: 4, col: j, sold: 0 });
 
   // row 5
   for (let j = 33; j >= 31; j -= 2) seats.push({ area: "X", row: 5, col: j, sold: 0 });
-  for (let j = 29; j >= 25; j -= 2) seats.push({ area: "B", row: 5, col: j, sold: 0 });
-  for (let j = 23; j >= 13; j -= 2) seats.push({ area: "A", row: 5, col: j, sold: 0 });
+  for (let j = 29; j >= 23; j -= 2) seats.push({ area: "B", row: 5, col: j, sold: 0 });
+  for (let j = 21; j >= 13; j -= 2) seats.push({ area: "A", row: 5, col: j, sold: 0 });
   seats.push({ area: "M", row: 5, col: 0, sold: 0 });
   for (let j = 0; j < 3; j++) seats.push({ area: "X", row: 5, col: j, sold: 0 });
   seats.push({ area: "M", row: 5, col: 0, sold: 0 });
@@ -126,14 +127,14 @@ function generate_seats() {
   seats.push({ area: "M", row: 5, col: 0, sold: 0 });
   for (let j = 0; j < 4; j++) seats.push({ area: "X", row: 5, col: j, sold: 0 });
   seats.push({ area: "M", row: 5, col: 0, sold: 0 });
-  for (let j = 12; j <= 22; j += 2) seats.push({ area: "A", row: 5, col: j, sold: 0 });
-  for (let j = 24; j <= 28; j += 2) seats.push({ area: "B", row: 5, col: j, sold: 0 });
+  for (let j = 12; j <= 20; j += 2) seats.push({ area: "A", row: 5, col: j, sold: 0 });
+  for (let j = 22; j <= 28; j += 2) seats.push({ area: "B", row: 5, col: j, sold: 0 });
   for (let j = 30; j <= 32; j += 2) seats.push({ area: "X", row: 5, col: j, sold: 0 });
 
   // row 6
   for (let j = 33; j >= 31; j -= 2) seats.push({ area: "X", row: 6, col: j, sold: 0 });
-  for (let j = 29; j >= 27; j -= 2) seats.push({ area: "B", row: 6, col: j, sold: 0 });
-  for (let j = 25; j >= 13; j -= 2) seats.push({ area: "A", row: 6, col: j, sold: 0 });
+  for (let j = 29; j >= 25; j -= 2) seats.push({ area: "B", row: 6, col: j, sold: 0 });
+  for (let j = 23; j >= 13; j -= 2) seats.push({ area: "A", row: 6, col: j, sold: 0 });
   seats.push({ area: "M", row: 6, col: 0, sold: 0 });
   for (let j = 0; j < 3; j++) seats.push({ area: "X", row: 6, col: j, sold: 0 });
   seats.push({ area: "M", row: 6, col: 0, sold: 0 });
@@ -142,14 +143,14 @@ function generate_seats() {
   seats.push({ area: "M", row: 6, col: 0, sold: 0 });
   for (let j = 0; j < 3; j++) seats.push({ area: "X", row: 6, col: j, sold: 0 });
   seats.push({ area: "M", row: 6, col: 0, sold: 0 });
-  for (let j = 14; j <= 26; j += 2) seats.push({ area: "A", row: 6, col: j, sold: 0 });
-  for (let j = 28; j <= 30; j += 2) seats.push({ area: "B", row: 6, col: j, sold: 0 });
+  for (let j = 14; j <= 24; j += 2) seats.push({ area: "A", row: 6, col: j, sold: 0 });
+  for (let j = 26; j <= 30; j += 2) seats.push({ area: "B", row: 6, col: j, sold: 0 });
   for (let j = 32; j <= 34; j += 2) seats.push({ area: "X", row: 6, col: j, sold: 0 });
 
   //row 7
   seats.push({ area: "X", row: 7, col: 35, sold: 0 });
-  for (let j = 33; j >= 29; j -= 2) seats.push({ area: "B", row: 7, col: j, sold: 0 });
-  for (let j = 27; j >= 15; j -= 2) seats.push({ area: "A", row: 7, col: j, sold: 0 });
+  for (let j = 33; j >= 27; j -= 2) seats.push({ area: "B", row: 7, col: j, sold: 0 });
+  for (let j = 25; j >= 15; j -= 2) seats.push({ area: "A", row: 7, col: j, sold: 0 });
   seats.push({ area: "M", row: 7, col: 0, sold: 0 });
   for (let j = 0; j < 2; j++) seats.push({ area: "X", row: 7, col: j, sold: 0 });
   seats.push({ area: "M", row: 7, col: 0, sold: 0 });
@@ -158,14 +159,14 @@ function generate_seats() {
   seats.push({ area: "M", row: 7, col: 0, sold: 0 });
   for (let j = 0; j < 3; j++) seats.push({ area: "X", row: 7, col: j, sold: 0 });
   seats.push({ area: "M", row: 7, col: 0, sold: 0 });
-  for (let j = 14; j <= 26; j += 2) seats.push({ area: "A", row: 7, col: j, sold: 0 });
-  for (let j = 28; j <= 32; j += 2) seats.push({ area: "B", row: 7, col: j, sold: 0 });
+  for (let j = 14; j <= 24; j += 2) seats.push({ area: "A", row: 7, col: j, sold: 0 });
+  for (let j = 26; j <= 32; j += 2) seats.push({ area: "B", row: 7, col: j, sold: 0 });
   seats.push({ area: "X", row: 7, col: 34, sold: 0 });
 
   //row 8
   seats.push({ area: "X", row: 8, col: 35, sold: 0 });
-  for (let j = 33; j >= 31; j -= 2) seats.push({ area: "B", row: 8, col: j, sold: 0 });
-  for (let j = 29; j >= 15; j -= 2) seats.push({ area: "A", row: 8, col: j, sold: 0 });
+  for (let j = 33; j >= 29; j -= 2) seats.push({ area: "B", row: 8, col: j, sold: 0 });
+  for (let j = 27; j >= 15; j -= 2) seats.push({ area: "A", row: 8, col: j, sold: 0 });
   seats.push({ area: "M", row: 8, col: 0, sold: 0 });
   for (let j = 0; j < 2; j++) seats.push({ area: "X", row: 8, col: j, sold: 0 });
   seats.push({ area: "M", row: 8, col: 0, sold: 0 });
@@ -174,13 +175,13 @@ function generate_seats() {
   seats.push({ area: "M", row: 8, col: 0, sold: 0 });
   for (let j = 0; j < 2; j++) seats.push({ area: "X", row: 8, col: j, sold: 0 });
   seats.push({ area: "M", row: 8, col: 0, sold: 0 });
-  for (let j = 16; j <= 30; j += 2) seats.push({ area: "A", row: 8, col: j, sold: 0 });
-  for (let j = 32; j <= 34; j += 2) seats.push({ area: "B", row: 8, col: j, sold: 0 });
+  for (let j = 16; j <= 28; j += 2) seats.push({ area: "A", row: 8, col: j, sold: 0 });
+  for (let j = 30; j <= 34; j += 2) seats.push({ area: "B", row: 8, col: j, sold: 0 });
   seats.push({ area: "X", row: 8, col: 36, sold: 0 });
 
   //row 9
-  for (let j = 37; j >= 33; j -= 2) seats.push({ area: "B", row: 9, col: j, sold: 0 });
-  for (let j = 31; j >= 17; j -= 2) seats.push({ area: "A", row: 9, col: j, sold: 0 });
+  for (let j = 37; j >= 31; j -= 2) seats.push({ area: "B", row: 9, col: j, sold: 0 });
+  for (let j = 29; j >= 17; j -= 2) seats.push({ area: "A", row: 9, col: j, sold: 0 });
   seats.push({ area: "M", row: 9, col: 0, sold: 0 });
   seats.push({ area: "X", row: 9, col: 0, sold: 0 });
   seats.push({ area: "M", row: 9, col: 0, sold: 0 });
@@ -189,16 +190,16 @@ function generate_seats() {
   seats.push({ area: "M", row: 9, col: 0, sold: 0 });
   for (let j = 0; j < 2; j++) seats.push({ area: "X", row: 9, col: j, sold: 0 });
   seats.push({ area: "M", row: 9, col: 0, sold: 0 });
-  for (let j = 16; j <= 30; j += 2) seats.push({ area: "A", row: 9, col: j, sold: 0 });
-  for (let j = 32; j <= 36; j += 2) seats.push({ area: "B", row: 9, col: j, sold: 0 });
+  for (let j = 16; j <= 28; j += 2) seats.push({ area: "A", row: 9, col: j, sold: 0 });
+  for (let j = 30; j <= 36; j += 2) seats.push({ area: "B", row: 9, col: j, sold: 0 });
 
   //row 10
   for (let j = 37; j >= 17; j -= 2) seats.push({ area: "B", row: 10, col: j, sold: 0 });
   seats.push({ area: "M", row: 10, col: 0, sold: 0 });
   seats.push({ area: "X", row: 10, col: 0, sold: 0 });
   seats.push({ area: "M", row: 10, col: 0, sold: 0 });
-  for (let j = 15; j >= 1; j -= 2) seats.push({ area: "B", row: 10, col: j, sold: 0 });
-  for (let j = 2; j <= 16; j += 2) seats.push({ area: "B", row: 10, col: j, sold: 0 });
+  for (let j = 15; j >= 1; j -= 2) seats.push({ area: "A", row: 10, col: j, sold: 0 });
+  for (let j = 2; j <= 16; j += 2) seats.push({ area: "A", row: 10, col: j, sold: 0 });
   seats.push({ area: "M", row: 10, col: 0, sold: 0 });
   seats.push({ area: "X", row: 10, col: 0, sold: 0 });
   seats.push({ area: "M", row: 10, col: 0, sold: 0 });
@@ -209,8 +210,8 @@ function generate_seats() {
   for (let j = 35; j >= 19; j -= 2) seats.push({ area: "B", row: 11, col: j, sold: 0 });
   seats.push({ area: "M", row: 11, col: 0, sold: 0 });
   seats.push({ area: "M", row: 11, col: 0, sold: 0 });
-  for (let j = 17; j >= 1; j -= 2) seats.push({ area: "B", row: 11, col: j, sold: 0 });
-  for (let j = 2; j <= 16; j += 2) seats.push({ area: "B", row: 11, col: j, sold: 0 });
+  for (let j = 17; j >= 1; j -= 2) seats.push({ area: "A", row: 11, col: j, sold: 0 });
+  for (let j = 2; j <= 16; j += 2) seats.push({ area: "A", row: 11, col: j, sold: 0 });
   seats.push({ area: "M", row: 11, col: 0, sold: 0 });
   seats.push({ area: "X", row: 11, col: 0, sold: 0 });
   seats.push({ area: "M", row: 11, col: 0, sold: 0 });
@@ -260,6 +261,94 @@ function generate_seats() {
   seats.push({ area: "M", row: 15, col: 0, sold: 0 });
   for (let j = 0; j < 11; j++) seats.push({ area: "X", row: 15, col: j, sold: 0 });
   // seats.reverse();
+
+  // second floor
+  //row 1
+  for (let j = 35; j >= 25; j -= 2) seats.push({ area: "X", row: 1, col: j, sold: 0 });
+  for (let j = 23; j >= 21; j -= 2) seats.push({ area: "S", row: 1, col: j, sold: 0 });
+  for (let j = 19; j >= 15; j -= 2) seats.push({ area: "C", row: 1, col: j, sold: 0 });
+  seats.push({ area: "M", row: 1, col: 0, sold: 0 });
+  for (let j = 0; j < 2; j++) seats.push({ area: "X", row: 1, col: j, sold: 0 });
+  seats.push({ area: "M", row: 1, col: 0, sold: 0 });
+  for (let j = 13; j >= 1; j -= 2) seats.push({ area: "C", row: 1, col: j, sold: 0 });
+  for (let j = 2; j <= 12; j += 2) seats.push({ area: "C", row: 1, col: j, sold: 0 });
+  seats.push({ area: "M", row: 1, col: 0, sold: 0 });
+  for (let j = 0; j < 3; j++) seats.push({ area: "X", row: 1, col: j, sold: 0 });
+  seats.push({ area: "M", row: 1, col: 0, sold: 0 });
+  for (let j = 14; j <= 18; j += 2) seats.push({ area: "C", row: 1, col: j, sold: 0 });
+  for (let j = 20; j <= 22; j += 2) seats.push({ area: "S", row: 1, col: j, sold: 0 });
+  for (let j = 24; j <= 34; j += 2) seats.push({ area: "X", row: 1, col: j, sold: 0 });
+
+  // row 2
+  for (let j = 29; j >= 27; j -= 2) seats.push({ area: "S", row: 2, col: j, sold: 0 });
+  for (let j = 0; j < 3; j++) seats.push({ area: "X", row: 2, col: j, sold: 0 });
+  for (let j = 25; j >= 21; j -= 2) seats.push({ area: "S", row: 2, col: j, sold: 0 });
+  for (let j = 19; j >= 15; j -= 2) seats.push({ area: "C", row: 2, col: j, sold: 0 });
+  seats.push({ area: "M", row: 2, col: 0, sold: 0 });
+  for (let j = 0; j < 2; j++) seats.push({ area: "X", row: 2, col: j, sold: 0 });
+  seats.push({ area: "M", row: 2, col: 0, sold: 0 });
+  for (let j = 13; j >= 1; j -= 2) seats.push({ area: "C", row: 2, col: j, sold: 0 });
+  for (let j = 2; j <= 14; j += 2) seats.push({ area: "C", row: 2, col: j, sold: 0 });
+  seats.push({ area: "M", row: 2, col: 0, sold: 0 });
+  for (let j = 0; j < 2; j++) seats.push({ area: "X", row: 2, col: j, sold: 0 });
+  seats.push({ area: "M", row: 2, col: 0, sold: 0 });
+  for (let j = 16; j <= 20; j += 2) seats.push({ area: "C", row: 2, col: j, sold: 0 });
+  for (let j = 22; j <= 26; j += 2) seats.push({ area: "S", row: 2, col: j, sold: 0 });
+  for (let j = 0; j < 3; j++) seats.push({ area: "X", row: 2, col: j, sold: 0 });
+  for (let j = 28; j <= 30; j += 2) seats.push({ area: "S", row: 2, col: j, sold: 0 });
+
+  // row 3
+  for (let j = 37; j >= 23; j -= 2) seats.push({ area: "X", row: 3, col: j, sold: 0 });
+  for (let j = 21; j >= 17; j -= 2) seats.push({ area: "C", row: 3, col: j, sold: 0 });
+  seats.push({ area: "M", row: 3, col: 0, sold: 0 });
+  seats.push({ area: "X", row: 3, col: 0, sold: 0 });
+  seats.push({ area: "M", row: 3, col: 0, sold: 0 });
+  for (let j = 15; j >= 1; j -= 2) seats.push({ area: "C", row: 3, col: j, sold: 0 });
+  for (let j = 2; j <= 14; j += 2) seats.push({ area: "C", row: 3, col: j, sold: 0 });
+  seats.push({ area: "M", row: 3, col: 0, sold: 0 });
+  for (let j = 0; j < 2; j++) seats.push({ area: "X", row: 3, col: j, sold: 0 });
+  seats.push({ area: "M", row: 3, col: 0, sold: 0 });
+  for (let j = 16; j <= 20; j += 2) seats.push({ area: "C", row: 3, col: j, sold: 0 });
+  for (let j = 22; j <= 36; j += 2) seats.push({ area: "X", row: 3, col: j, sold: 0 });
+
+  for (let j = 0; j < 44; j++) seats.push({ area: "X", row: 0, col: j, sold: 0 });
+
+  //row 4
+  for (let j = 31; j >= 15; j -= 2) seats.push({ area: "X", row: 4, col: j, sold: 0 });
+  for (let j = 13; j >= 11; j -= 2) seats.push({ area: "S", row: 4, col: j, sold: 0 });
+  seats.push({ area: "M", row: 4, col: 0, sold: 0 });
+  for (let j = 0; j < 4; j++) seats.push({ area: "X", row: 4, col: j, sold: 0 });
+  seats.push({ area: "M", row: 4, col: 0, sold: 0 });
+  for (let j = 9; j >= 1; j -= 2) seats.push({ area: "S", row: 4, col: j, sold: 0 });
+  for (let j = 2; j <= 6; j += 2) seats.push({ area: "S", row: 4, col: j, sold: 0 });
+  seats.push({ area: "M", row: 4, col: 0, sold: 0 });
+  for (let j = 0; j < 6; j++) seats.push({ area: "X", row: 4, col: j, sold: 0 });
+  seats.push({ area: "M", row: 4, col: 0, sold: 0 });
+  for (let j = 8; j <= 10; j += 2) seats.push({ area: "S", row: 4, col: j, sold: 0 });
+  for (let j = 12; j <= 28; j += 2) seats.push({ area: "X", row: 4, col: j, sold: 0 });
+
+  //row 5
+  for (let j = 31; j >= 17; j -= 2) seats.push({ area: "X", row: 5, col: j, sold: 0 });
+  for (let j = 15; j >= 11; j -= 2) seats.push({ area: "S", row: 5, col: j, sold: 0 });
+  seats.push({ area: "M", row: 5, col: 0, sold: 0 });
+  for (let j = 0; j < 4; j++) seats.push({ area: "X", row: 5, col: j, sold: 0 });
+  seats.push({ area: "M", row: 5, col: 0, sold: 0 });
+  for (let j = 9; j >= 1; j -= 2) seats.push({ area: "S", row: 5, col: j, sold: 0 });
+  for (let j = 2; j <= 8; j += 2) seats.push({ area: "S", row: 5, col: j, sold: 0 });
+  seats.push({ area: "M", row: 5, col: 0, sold: 0 });
+  for (let j = 0; j < 5; j++) seats.push({ area: "X", row: 5, col: j, sold: 0 });
+  seats.push({ area: "M", row: 5, col: 0, sold: 0 });
+  for (let j = 10; j <= 14; j += 2) seats.push({ area: "S", row: 5, col: j, sold: 0 });
+  for (let j = 16; j <= 30; j += 2) seats.push({ area: "X", row: 5, col: j, sold: 0 });
+
+  //row 6
+  for (let j = 31; j >= 15; j -= 2) seats.push({ area: "X", row: 6, col: j, sold: 0 });
+  for (let j = 13; j >= 11; j -= 2) seats.push({ area: "S", row: 6, col: j, sold: 0 });
+  seats.push({ area: "M", row: 6, col: 0, sold: 0 });
+  for (let j = 0; j < 20; j++) seats.push({ area: "X", row: 6, col: j, sold: 0 });
+  seats.push({ area: "M", row: 6, col: 0, sold: 0 });
+  for (let j = 10; j <= 12; j += 2) seats.push({ area: "S", row: 6, col: j, sold: 0 });
+  for (let j = 14; j <= 30; j += 2) seats.push({ area: "X", row: 6, col: j, sold: 0 });
 
   return seats;
 }
